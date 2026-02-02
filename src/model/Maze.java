0405eb50ccee9c8fc2681828916404329aff42ea
dostonme <dostonme@rose-hulman.edu>
2026-02-01 22:49:27 -0500

    I added another image since the previous one doen't work with the ground for the  maze and I developed the maze class a little bit more.

--- src/model/Maze.java
+++ src/model/Maze.java
@@ -1,12 +1,32 @@
 package model;
 
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+
 
 public class Maze extends GameModel{
 
 	public Maze(int x, int y, int height, int width, BufferedImage sprite) {
 		super(x, y, height, width, sprite);
 		// TODO Auto-generated constructor stub
+		
+	}
+	
+	public void groundMaze(Graphics2D g2, BufferedImage sprite) {
+		try {
+			sprite = ImageIO.read(Maze.class.getResource("minecraft grass block.webp"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		super.draw(g2);
+		
+		
 	}
 	
 	
--- src/ui/GameComponent.java
+++ src/ui/GameComponent.java
@@ -4,12 +4,19 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Shape;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
 
+import javax.imageio.ImageIO;
 import javax.swing.JComponent;
 
 import model.GameModel;
+import model.Maze;
 import model.PlayerSprite;
 import model.ZombieSprite;
+
+
 
 public class GameComponent extends JComponent {
 
@@ -17,14 +24,21 @@ public class GameComponent extends JComponent {
 	
 //	private GameModel model;
 	private PlayerSprite player;
-
+	private static BufferedImage ground = null;
+	private static boolean triedLoad = false;
 
 //	public GameComponent(GameModel model) {
 //		this.model = model;
 //	}
 	public GameComponent(PlayerSprite player) {
 		this.player= player;
+	
 	}
+
+
+	
+	
+
 
 
 	@Override
@@ -35,8 +49,10 @@ public class GameComponent extends JComponent {
 	// Minimal placeholder to test  it’s running
 //	g2.drawString("Final Project Starter: UI is running ✅", 20, 30);
 	
-	player.draw(g2);
-
+		
+		
+		player.draw(g2);
+	
 	// TODO: draw based on model state
 		
 	}
